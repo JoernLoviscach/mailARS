b141c9ae94bc879ec9a1d9c669805a81d3eefcca
Wincent Balin <wincent.balin@gmail.com>
2022-02-16 21:42:24 +0100

    Added Russian translation
    Translations followed Mozilla Thunderbird  (https://hg.mozilla.org/l10n-central/ru and https://context.reverso.net/translation/english-russian/) mostly and the drawing-related translations followed GIMP (https://gitlab.gnome.org/GNOME/gimp/-/blob/master/po/ru.po?expanded=true&viewer=simple).

---
+++ translation_ru.ts
@@ -0,0 +1,431 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru" sourcelanguage="en">
+<context>
+    <name>CameraCapture</name>
+    <message>
+        <location filename="camera_capture.py" line="33"/>
+        <source>Camera is missing or not already in use.</source>
+        <translation>Камера отсутствует или уже используется.</translation>
+    </message>
+</context>
+<context>
+    <name>DrawingWidget</name>
+    <message>
+        <location filename="drawing_widget.py" line="120"/>
+        <source>Safety prompt</source>
+        <translation>Подтверждение удаления</translation>
+    </message>
+    <message>
+        <location filename="drawing_widget.py" line="120"/>
+        <source>Delete all?</source>
+        <translation>Удалить всё?</translation>
+    </message>
+</context>
+<context>
+    <name>DrawingWindow</name>
+    <message>
+        <location filename="drawing_window.py" line="58"/>
+        <source>Addressees</source>
+        <translation>Адресаты... ▹ A</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="50"/>
+        <source>Send</source>
+        <translation>Отправить ▹ Strg+⏎</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="65"/>
+        <source>Undo</source>
+        <translation>Отменить ▹ Z</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="73"/>
+        <source>Redo</source>
+        <translation>Повторить ▹ Y</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="86"/>
+        <source>Drawing:</source>
+        <translation>Рисунок:</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="94"/>
+        <source>Draw</source>
+        <translation>Рисовать ▹ Q</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="107"/>
+        <source>Color</source>
+        <translation>Цвет линии... ▹ S</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="124"/>
+        <source>Thickness</source>
+        <translatorcomment>Как в GIMP</translatorcomment>
+        <translation>Толщина линии ▹ D</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="133"/>
+        <source>Erase</source>
+        <translation>Стереть ▹ W</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="146"/>
+        <source>Text:</source>
+        <translation>Текст:</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="150"/>
+        <source>Type</source>
+        <translation>Печатать ▹ E</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="168"/>
+        <source>Size</source>
+        <translation>Размер шрифта ▹ F</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="180"/>
+        <source>Photo:</source>
+        <translation>Фото:</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="184"/>
+        <source>Take Photo</source>
+        <translation>Сделать фото ▹ G</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="196"/>
+        <source>Selection:</source>
+        <translation>Выделение:</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="200"/>
+        <source>Select</source>
+        <translation>Выделить ▹ R</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="208"/>
+        <source>Delete</source>
+        <translation>Удалить ▹ Entf</translation>
+    </message>
+    <message>
+        <location filename="drawing_window.py" line="395"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+</context>
+<context>
+    <name>Internal Error</name>
+    <message>
+        <location filename="__main__.py" line="54"/>
+        <source>Internal Error</source>
+        <translation>Внутренняя ошибка</translation>
+    </message>
+    <message>
+        <location filename="__main__.py" line="54"/>
+        <source>Please take a screenshot of this:</source>
+        <translation>Пожалуйста, сделайте скриншот этого диалога:</translation>
+    </message>
+</context>
+<context>
+    <name>MailboxWidget</name>
+    <message>
+        <location filename="mailbox_widget.py" line="192"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="mailbox_widget.py" line="61"/>
+        <source>File error:</source>
+        <translation>Ошибка в файле:</translation>
+    </message>
+    <message>
+        <location filename="mailbox_widget.py" line="160"/>
+        <source>Information</source>
+        <translation>Уведомление</translation>
+    </message>
+    <message>
+        <location filename="mailbox_widget.py" line="160"/>
+        <source>Nothing selected for export!</source>
+        <translation>Ничего не выделено для экспорта!</translation>
+    </message>
+</context>
+<context>
+    <name>MailboxesWindow</name>
+    <message>
+        <location filename="mailboxes_window.py" line="41"/>
+        <source>Inbox</source>
+        <translation>Входящие</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="44"/>
+        <source>Drafts</source>
+        <translation>Черновики</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="47"/>
+        <source>Outbox</source>
+        <translation>Отправленные</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="53"/>
+        <source>Fetch mail</source>
+        <translation>Загрузить сообщения</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="60"/>
+        <source>Only show latest mail of every sender</source>
+        <translation>Показывать только последнее сообщение каждого отправителя</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="71"/>
+        <source>New draft</source>
+        <translation>Новый черновик</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="82"/>
+        <source>Export as PDF</source>
+        <translation>Экспортировать в формате PDF</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="118"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="163"/>
+        <source>Safety prompt</source>
+        <translation>Подтверждение выхода</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="163"/>
+        <source>Do you really want to quit?</source>
+        <translation>Вы действительно хотите выйти?</translation>
+    </message>
+    <message>
+        <location filename="mailboxes_window.py" line="107"/>
+        <source>Number of columns</source>
+        <translation>Количество столбцов</translation>
+    </message>
+</context>
+<context>
+    <name>Password</name>
+    <message>
+        <location filename="preferences.py" line="58"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="58"/>
+        <source>Mail password</source>
+        <translation>Пароль электронной почты</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="preferences.py" line="112"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="116"/>
+        <source>Language</source>
+        <translation>Язык пользовательского интерфейса</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="136"/>
+        <source>Changes are applied on next start</source>
+        <translation>Перезапустите mailARS для смены языка</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="138"/>
+        <source>Mail Settings</source>
+        <translation>Настройки электронной почты</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="146"/>
+        <source>Name to be displayed</source>
+        <translation>Выводимое имя</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="149"/>
+        <source>SMTP sender</source>
+        <translation>Отправитель SMTP-сообщений</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="152"/>
+        <source>IMAP user</source>
+        <translation>Имя пользователя IMAP</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="156"/>
+        <source>SMTP receiver</source>
+        <translation>Получатель SMTP-сообщений</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="157"/>
+        <source>Leave empty to only react to incoming mails</source>
+        <translation>Оставить пустым, чтобы реагировать только на входящие сообщения</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="161"/>
+        <source>SMTP server</source>
+        <translation>Сервер SMTP</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="164"/>
+        <source>IMAP server</source>
+        <translation>Сервер IMAP</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="172"/>
+        <source>Smooth curves after drawing</source>
+        <translation>Сглаживание кривых после рисования</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="180"/>
+        <source>Change mail password...</source>
+        <translation>Сменить пароль электронной почты...</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="166"/>
+        <source>Miscellaneous</source>
+        <translation>Разное</translation>
+    </message>
+</context>
+<context>
+    <name>Sender</name>
+    <message>
+        <location filename="mailing.py" line="93"/>
+        <source>SMTP settings are incomplete.</source>
+        <translation>Настройки SMTP неполные.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="95"/>
+        <source>No addressees available</source>
+        <translation>Адресаты отсутствуют</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="97"/>
+        <source>Sending</source>
+        <translation>Отправка</translation>
+    </message>
+</context>
+<context>
+    <name>Thumbnail</name>
+    <message>
+        <location filename="thumbnail.py" line="247"/>
+        <source>Error when writing message</source>
+        <translation>Ошибка при записи сообщения</translation>
+    </message>
+    <message>
+        <location filename="thumbnail.py" line="287"/>
+        <source>Error when removing directory</source>
+        <translation>Ошибка при удалении папки</translation>
+    </message>
+    <message>
+        <location filename="thumbnail.py" line="287"/>
+        <source>File Error</source>
+        <translation>Ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>_ReceiverThread</name>
+    <message>
+        <location filename="mailing.py" line="227"/>
+        <source>IMAP List failed.</source>
+        <translation>Запрос IMAP List не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="231"/>
+        <source>IMAP Create failed.</source>
+        <translation>Запрос IMAP Create не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="256"/>
+        <source>IMAP Search failed.</source>
+        <translation>Запрос IMAP Search не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="265"/>
+        <source>IMAP Peek failed.</source>
+        <translation>Запрос IMAP Peek не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="284"/>
+        <source>IMAP Fetch failed.</source>
+        <translation>Запрос IMAP Fetch не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="290"/>
+        <source>(Error in address)</source>
+        <translation>(ошибка в адресе)</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="320"/>
+        <source>IMAP UID Copy failed.</source>
+        <translation>Запрос IMAP UID Copy не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="323"/>
+        <source>IMAP UID Store failed.</source>
+        <translation>Запрос IMAP UID Store не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="327"/>
+        <source>IMAP Expunge failed.</source>
+        <translation>Запрос IMAP Expunge не удался.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="363"/>
+        <source>IMAP settings are incomplete.</source>
+        <translation>Настройки IMAP неполные.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="366"/>
+        <source>The mail password cannot be empty.</source>
+        <translation>Пароль электронной почты не может быть пустым.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="260"/>
+        <source>Fetching mail:</source>
+        <translation>Загрузка сообщений:</translation>
+    </message>
+</context>
+<context>
+    <name>_SenderRunnable</name>
+    <message>
+        <location filename="mailing.py" line="92"/>
+        <source>SMTP settings are incomplete.</source>
+        <translation type="obsolete">Einstellungen für SMTP sind unvollständig.</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="94"/>
+        <source>No addressees available</source>
+        <translation type="obsolete">Keine Empfänger verfügbar</translation>
+    </message>
+    <message>
+        <location filename="mailing.py" line="96"/>
+        <source>Sending</source>
+        <translation type="obsolete">Beim Senden</translation>
+    </message>
+</context>
+<context>
+    <name>app</name>
+    <message>
+        <location filename="preferences.py" line="58"/>
+        <source>Password</source>
+        <comment>Password</comment>
+        <translation type="obsolete">Passwort</translation>
+    </message>
+    <message>
+        <location filename="preferences.py" line="58"/>
+        <source>Password</source>
+        <comment>Mail password</comment>
+        <translation type="obsolete">Passwort</translation>
+    </message>
+</context>
+</TS>
