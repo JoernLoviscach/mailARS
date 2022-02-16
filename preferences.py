bd89c882ddb67504e930479488ab34ff7048568d
Wincent Balin <wincent.balin@gmail.com>
2022-02-16 21:43:32 +0100

    Added Russian language to preferences dialog

--- preferences.py
+++ preferences.py
@@ -132,6 +132,10 @@ class PreferencesDialog(widgets.QDialog):
         self._radio_german.setText("de")
         self._radio_german.setChecked(get("language") == "de")
         language_group_radios_layout.addWidget(self._radio_german, core.Qt.AlignLeft)
+        self._radio_russian = widgets.QRadioButton()
+        self._radio_russian.setText("ru")
+        self._radio_russian.setChecked(get("language") == "ru")
+        language_group_radios_layout.addWidget(self._radio_russian, core.Qt.AlignLeft)
 
         language_group_outer_layout.addWidget(widgets.QLabel(self.tr("Changes are applied on next start")))
 
@@ -193,6 +197,8 @@ class PreferencesDialog(widgets.QDialog):
         lang = "en"
         if self._radio_german.isChecked():
             lang = "de"
+        if self._radio_russian.isChecked():
+            lang = "ru"
         set("language", lang)
         set("name", self._name.text().strip())
         set("smtp_sender", self._smtp_sender.text().strip())
